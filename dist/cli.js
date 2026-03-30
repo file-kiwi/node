7491052623f1545af5debab5df548ca39f1a7b0b
cyflu <cyflu@DESKTOP-NAHKO36>
2026-03-31 00:31:33 +0900

    feat: implement CLI for filekiwi with upload and progress tracking support

--- dist/cli.js
+++ dist/cli.js
@@ -67,7 +67,7 @@ function calcWidth(fileStates) {
         if (w > max)
             max = w;
     }
-    return max;
+    return Math.max(max, 30);
 }
 function renderFileLine(f, width) {
     const name = truncName(f.name);
@@ -99,10 +99,11 @@ function drawProgress(fileStates, url, retentionHours, lineCount) {
     }
     process.stdout.write(`${CLEAR_LINE}\n`);
     process.stdout.write(`${CLEAR_LINE}  ${url}\n`);
+    process.stdout.write(`${CLEAR_LINE}  \x1b[2mYou can share this link even while uploading.\x1b[0m\n`);
     process.stdout.write(`${CLEAR_LINE}\n`);
     process.stdout.write(`${CLEAR_LINE}  \x1b[2m[ ] = free download hours left\x1b[0m\n`);
     process.stdout.write(`${CLEAR_LINE}  \x1b[2mAll files will be deleted from the server after ${retentionHours} hours.\x1b[0m\n`);
-    count += 5;
+    count += 6;
     return count;
 }
 function makeCallbacks(webfolder, fileStates, lineCountRef) {
@@ -181,7 +182,7 @@ async function main() {
         });
         process.stdout.write(SHOW_CURSOR);
         fs.unlinkSync(tmp);
-        console.log(`\n  Done. Resume complete.\n`);
+        console.log(`\n  Done. ${webfolder.files.length} file(s) uploaded.\n`);
     }
     else if (filePaths.length > 0) {
         const webfolder = await createWebFolder({
